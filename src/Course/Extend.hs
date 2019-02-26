8212fae51b83350f4b65595d3af9b4165c84d06b
Gabriel Torello <gabriel128@gmail.com>
2019-02-26 20:28:54 +1100

    Add Extend

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -17,10 +17,7 @@ import Course.Functor
 --   `∀f g. (f <<=) . (g <<=) ≅ (<<=) (f . (g <<=))`
 class Functor f => Extend f where
   -- Pronounced, extend.
-  (<<=) ::
-    (f a -> b)
-    -> f a
-    -> f b
+  (<<=) :: (f a -> b) -> f a -> f b
 
 infixr 1 <<=
 
@@ -29,12 +26,8 @@ infixr 1 <<=
 -- >>> id <<= ExactlyOne 7
 -- ExactlyOne (ExactlyOne 7)
 instance Extend ExactlyOne where
-  (<<=) ::
-    (ExactlyOne a -> b)
-    -> ExactlyOne a
-    -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  (<<=) :: (ExactlyOne a -> b) -> ExactlyOne a -> ExactlyOne b
+  f <<= oneA =  ExactlyOne $ f oneA
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -47,12 +40,11 @@ instance Extend ExactlyOne where
 -- >>> reverse <<= ((1 :. 2 :. 3 :. Nil) :. (4 :. 5 :. 6 :. Nil) :. Nil)
 -- [[[4,5,6],[1,2,3]],[[4,5,6]]]
 instance Extend List where
-  (<<=) ::
-    (List a -> b)
-    -> List a
-    -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  (<<=) :: (List a -> b) -> List a -> List b
+  f <<= listA = unfoldr anaF listA
+     where
+       anaF Nil = Empty
+       anaF a@(_ :. xs) = Full (f a, xs)
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -62,12 +54,9 @@ instance Extend List where
 -- >>> id <<= Empty
 -- Empty
 instance Extend Optional where
-  (<<=) ::
-    (Optional a -> b)
-    -> Optional a
-    -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  (<<=) :: (Optional a -> b) -> Optional a -> Optional b
+  f <<= a@(Full _) = Full $ f a
+  _ <<= Empty = Empty
 
 -- | Duplicate the functor using extension.
 --
@@ -82,9 +71,5 @@ instance Extend Optional where
 --
 -- >>> cojoin Empty
 -- Empty
-cojoin ::
-  Extend f =>
-  f a
-  -> f (f a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin :: Extend f => f a -> f (f a)
+cojoin = (<<=) id
